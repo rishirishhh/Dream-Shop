ce902aab4e056e7fcd0ebcf58d377f3845fab52d
rishirishhh <rishikavya19@gmail.com>
2024-12-30 02:57:13 +0530

    first commit

---
+++ src/test/java/com/ecom/dreamshop/DreamshopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ecom.dreamshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DreamshopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
